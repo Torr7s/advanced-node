33380b395f0a1c0a214e0bf4ba4f10cabb63dd20
Matheus <torresmiranda.rj@gmail.com>
2023-04-12 14:16:45 -0300

    test: ensure UseCase returns AuthenticationError when LoadFacebookUserApi returns undefined

--- tests/data/use-cases/facebook-authentication.spec.ts
+++ tests/data/use-cases/facebook-authentication.spec.ts
@@ -1,28 +1,42 @@
+import { AuthenticationError } from '@/domain/errors/authentication';
 import { type FacebookAuthentication } from '@/domain/features';
-
-interface LoadFacebookUserApi {
-	exec: (input: LoadFacebookUserApi.Input) => Promise<void>;
-}
-
-export class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
-	public token?: string;
-
-	public async exec(input: LoadFacebookUserApi.Input): Promise<void> {
-		this.token = input.token;
-	}
-}
 
 export namespace LoadFacebookUserApi {
 	export type Input = {
 		token: string;
 	};
+
+	export type Output = undefined;
+}
+
+interface LoadFacebookUserApi {
+	exec: (
+		input: LoadFacebookUserApi.Input,
+	) => Promise<LoadFacebookUserApi.Output>;
+}
+
+export class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
+	public token?: string;
+	public output = undefined;
+
+	public async exec(
+		input: LoadFacebookUserApi.Input,
+	): Promise<LoadFacebookUserApi.Output> {
+		this.token = input.token;
+
+		return this.output;
+	}
 }
 
 class FacebookAuthenticationUseCase {
 	constructor(private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
 
-	public async exec(input: FacebookAuthentication.Input): Promise<void> {
+	public async exec(
+		input: FacebookAuthentication.Input,
+	): Promise<AuthenticationError> {
 		await this.loadFacebookUserApi.exec(input);
+
+		return new AuthenticationError();
 	}
 }
 
@@ -36,5 +50,18 @@ describe('FacebookAuthenticationUseCase', (): void => {
 		});
 
 		expect(loadFacebookUserApi.token).toBe('fake_random_token');
+	});
+
+	it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async (): Promise<void> => {
+		const loadFacebookUserApi = new LoadFacebookUserApiSpy();
+		loadFacebookUserApi.output = undefined;
+
+		const sut = new FacebookAuthenticationUseCase(loadFacebookUserApi);
+
+		const authOutput = await sut.exec({
+			token: 'fake_random_token',
+		});
+
+		expect(authOutput).toEqual(new AuthenticationError());
 	});
 });
