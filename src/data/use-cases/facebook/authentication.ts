740eb495f3b51023c988462f91bf7093199277eb
Matheus <torresmiranda.rj@gmail.com>
2023-04-14 10:16:40 -0300

    feat: inject LoadUserAccountRepository and find user by his facebook email

--- src/data/use-cases/facebook/authentication.ts
+++ src/data/use-cases/facebook/authentication.ts
@@ -1,15 +1,25 @@
 import { type LoadFacebookUserApi } from '@/data/contracts/apis';
+import { type LoadUserAccountRepository } from '@/data/contracts/repositories';
 
 import { AuthenticationError } from '@/domain/errors/authentication';
 import { type FacebookAuthentication } from '@/domain/features';
 
 export class FacebookAuthenticationUseCase {
-	constructor(private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
+	constructor(
+		private readonly loadFacebookUserApi: LoadFacebookUserApi,
+		private readonly loadUserAccountRepository: LoadUserAccountRepository,
+	) {}
 
 	public async exec(
 		input: FacebookAuthentication.Input,
 	): Promise<AuthenticationError> {
-		await this.loadFacebookUserApi.exec(input);
+		const fbData = await this.loadFacebookUserApi.exec(input);
+
+		if (fbData != null) {
+			await this.loadUserAccountRepository.findOne({
+				email: fbData.email,
+			});
+		}
 
 		return new AuthenticationError();
 	}
