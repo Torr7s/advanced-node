957b5096b1b79f43b22e941ceeb0056e16566232
Matheus <torresmiranda.rj@gmail.com>
2023-04-14 14:19:37 -0300

    feat: create TokenGenerator contract

---
+++ src/data/contracts/crypto/token.ts
@@ -0,0 +1,9 @@
+export interface TokenGenerator {
+	generateToken: (params: TokenGenerator.Input) => Promise<void>;
+}
+
+export namespace TokenGenerator {
+	export type Input = {
+		key: string;
+	};
+}
