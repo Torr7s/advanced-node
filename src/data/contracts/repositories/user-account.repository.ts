c7846906a7ce4d4c3f2738378df61f80804b12d6
Matheus <torresmiranda.rj@gmail.com>
2023-04-14 10:33:15 -0300

    feat: create CreateFacebookAccountRepository

--- src/data/contracts/repositories/user-account.repository.ts
+++ src/data/contracts/repositories/user-account.repository.ts
@@ -1,9 +1,27 @@
 export interface LoadUserAccountRepository {
-	findOne: (input: LoadUserAccountRepository.Input) => Promise<void>;
+	findOne: (
+		input: LoadUserAccountRepository.Input,
+	) => Promise<LoadUserAccountRepository.Output>;
 }
 
 export namespace LoadUserAccountRepository {
 	export type Input = {
+		email: string;
+	};
+
+	export type Output = undefined;
+}
+
+export interface CreateFacebookAccountRepository {
+	createFromFacebook: (
+		input: CreateFacebookAccountRepository.Input,
+	) => Promise<void>;
+}
+
+export namespace CreateFacebookAccountRepository {
+	export type Input = {
+		facebookId: string;
+		name: string;
 		email: string;
 	};
 }
