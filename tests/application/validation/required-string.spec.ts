830d7eb6cc15d65e3523112650d0fa77c2defa41
Matheus <torresmiranda.rj@gmail.com>
2023-04-18 09:27:24 -0300

    ensure RequiredStringValidator returns Error if value is empty

---
+++ tests/application/validation/required-string.spec.ts
@@ -0,0 +1,38 @@
+import { RequiredFieldError } from '@/application/errors';
+
+export class RequiredStringValidator {
+	constructor(
+		private readonly fieldName: string,
+		private readonly value: string,
+	) {}
+
+	public validate(): Error | undefined {
+		return new RequiredFieldError(this.fieldName);
+	}
+}
+
+describe('RequiredStringValidator', (): void => {
+	it('should return RequiredFieldError if value is empty', (): void => {
+		const sut = new RequiredStringValidator('any_field', '');
+
+		const error = sut.validate();
+
+		expect(error).toEqual(new RequiredFieldError('any_field'));
+	});
+
+	it('should return RequiredFieldError if value is null', (): void => {
+		const sut = new RequiredStringValidator('any_field', null as any);
+
+		const error = sut.validate();
+
+		expect(error).toEqual(new RequiredFieldError('any_field'));
+	});
+
+	it('should return RequiredFieldError if value is undefined', (): void => {
+		const sut = new RequiredStringValidator('any_field', undefined as any);
+
+		const error = sut.validate();
+
+		expect(error).toEqual(new RequiredFieldError('any_field'));
+	});
+});
