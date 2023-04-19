866fbca3115099b7fda5bdeeb2c8c91a1a0c0c74
Matheus <torresmiranda.rj@gmail.com>
2023-04-19 14:59:26 -0300

    move ValidationBuilder to its own file

---
+++ src/application/validation/builder.ts
@@ -0,0 +1,31 @@
+import {
+	RequiredStringValidator,
+	type Validator,
+} from '@/application/validation';
+
+export class ValidationBuilder {
+	private constructor(
+		private readonly fieldName: string,
+		private readonly value: string,
+		private readonly validators: Validator[] = [],
+	) {}
+
+	public static of(params: {
+		fieldName: string;
+		value: string;
+	}): ValidationBuilder {
+		return new ValidationBuilder(params.fieldName, params.value);
+	}
+
+	public required(): ValidationBuilder {
+		this.validators.push(
+			new RequiredStringValidator(this.fieldName, this.value),
+		);
+
+		return this;
+	}
+
+	public build(): Validator[] {
+		return this.validators;
+	}
+}
--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,34 +1,7 @@
 import {
 	RequiredStringValidator,
-	type Validator,
+	ValidationBuilder,
 } from '@/application/validation';
-
-export class ValidationBuilder {
-	private constructor(
-		private readonly fieldName: string,
-		private readonly value: string,
-		private readonly validators: Validator[] = [],
-	) {}
-
-	public static of(params: {
-		fieldName: string;
-		value: string;
-	}): ValidationBuilder {
-		return new ValidationBuilder(params.fieldName, params.value);
-	}
-
-	public required(): ValidationBuilder {
-		this.validators.push(
-			new RequiredStringValidator(this.fieldName, this.value),
-		);
-
-		return this;
-	}
-
-	public build(): Validator[] {
-		return this.validators;
-	}
-}
 
 describe('ValidationBuilder', (): void => {
 	it('should return a RequiredStringValidator', (): void => {
