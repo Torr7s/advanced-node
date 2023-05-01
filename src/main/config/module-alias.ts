ecc2c80e4f688ee538fbe067b2c46c80c62f4814
Matheus <torresmiranda.rj@gmail.com>
2023-05-01 17:55:24 -0300

    feat: add aliases for paths

--- src/main/config/module-alias.ts
+++ src/main/config/module-alias.ts
@@ -2,4 +2,12 @@ import path from 'node:path';
 
 import moduleAlias from 'module-alias';
 
-moduleAlias.addAlias('@', path.resolve('dist'));
+const files: string = path.resolve(__dirname, '../../..');
+
+moduleAlias.addAliases({
+	'@app': path.join(files, 'dist/application'),
+	'@data': path.join(files, 'dist/data'),
+	'@domain': path.join(files, 'dist/domain'),
+	'@infra': path.join(files, 'dist/infra'),
+	'@main': path.join(files, 'dist/main'),
+});
