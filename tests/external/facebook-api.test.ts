5bd5b388707bc7ae8b9af2fdc9a783104aa51ee5
Matheus <torresmiranda.rj@gmail.com>
2023-05-01 14:34:05 -0300

    isolate SUT creation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -4,15 +4,20 @@ import { AxiosHttpClient } from '@infra/http';
 import { env } from '@main/config/env';
 
 describe('Facebook Api Integration Tests', (): void => {
-	it('should return a Facebook User if token is valid', async (): Promise<void> => {
-		const axiosClient = new AxiosHttpClient();
+	let axiosClient: AxiosHttpClient;
+	let sut: FacebookAPI;
 
-		const sut = new FacebookAPI(
+	beforeEach((): void => {
+		axiosClient = new AxiosHttpClient();
+
+		sut = new FacebookAPI(
 			axiosClient,
 			env.facebookApi.clientId,
 			env.facebookApi.clientSecret,
 		);
+	});
 
+	it('should return a Facebook User if token is valid', async (): Promise<void> => {
 		/**
 		 * The expiration time of this token is in 3 months from now, so that is the reason for not
 		 * running this test together with the unit tests
@@ -29,14 +34,6 @@ describe('Facebook Api Integration Tests', (): void => {
 	});
 
 	it('should return undefined if token is invalid', async (): Promise<void> => {
-		const axiosClient = new AxiosHttpClient();
-
-		const sut = new FacebookAPI(
-			axiosClient,
-			env.facebookApi.clientId,
-			env.facebookApi.clientSecret,
-		);
-
 		const fbUser = await sut.loadUser({ token: 'invalid_token' });
 
 		expect(fbUser).toBeUndefined();
